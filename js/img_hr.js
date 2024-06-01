50c3840c81505738b34c13815d40f7b8a987da5a
unknown <laurasc1603@gmail.com>
2024-05-31 21:39:07 -0300

    att

--- js/img_hr.js
+++ js/img_hr.js
@@ -8,14 +8,17 @@ function hora(){
     if ((hr >= 5) && (hr < 12)){
         img.src = 'img/bom-dia.jpg'
         document.querySelector("#h3").innerHTML = "Bom dia!"
+        document.body.style.backgroundColor = "#74C5FF"
     }
     else if ((hr >= 12) && (hr <= 18)){
         img.src = 'img/boa-tarde.jpg'
         document.querySelector("#h3").innerHTML = "Boa tarde!"
+        document.body.style.backgroundColor = "#FFA25FB9"
     }
     else{
         img.src = 'img/boa-noite.jpg'
         document.querySelector("#h3").innerHTML = "Boa noite!"
+        document.body.style.backgroundColor = "#3B3B7A"
     }
 
     if (hr < 10){
